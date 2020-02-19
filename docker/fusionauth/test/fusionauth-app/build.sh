4838c1bd9c0d4a6a71326e17ebe02e35375886aa
Tyler Scott <tyduptyler13@gmail.com>
2020-02-19 10:48:12 -0700

    Fixed permission issue on docker containers
    Fixes https://github.com/FusionAuth/fusionauth-containers/issues/31
    Adopted changes from https://github.com/FusionAuth/fusionauth-containers/pull/29

--- docker/fusionauth/test/fusionauth-app/build.sh
+++ docker/fusionauth/test/fusionauth-app/build.sh
@@ -2,7 +2,7 @@
 
 currentDir=`pwd`
 cd ~/dev/inversoft/fusionauth/fusionauth-app
-sb bundle
+sb bundle --zipOnly
 cd ${currentDir}
 cp ~/dev/inversoft/fusionauth/fusionauth-app/build/bundles/fusionauth-app-*.zip .
 docker build -t fusionauth/fusionauth-app:dev .
