37741d63c6926028641b37ca5c1ef0c778dbe318
Daniel DeGroff <daniel@fusionauth.io>
2020-08-28 21:23:03 -0600

    squash merge stateless-oauth to master

--- docker/fusionauth/test/fusionauth-app/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd ~/dev/inversoft/fusionauth/fusionauth-app || exit 1
-sb bundle --zipOnly
-cd - || exit 1
-rm -rf ./fusionauth-app
-unzip ~/dev/inversoft/fusionauth/fusionauth-app/build/bundles/fusionauth-app-*.zip -d ./fusionauth-app
-docker build -t fusionauth/fusionauth-app:dev .
