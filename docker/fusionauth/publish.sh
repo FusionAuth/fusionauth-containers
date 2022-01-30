22a19cfc6e1473040181d618780bb0f5d8bb9650
Daniel DeGroff <daniel@fusionauth.io>
2022-01-30 15:02:30 -0700

    Working

--- docker/fusionauth/publish.sh
+++ docker/fusionauth/publish.sh
@@ -1,10 +1,10 @@
+#!/usr/bin/env bash
 
 echo "version=${1}" > release-trigger
+echo "preRelease=${2}" >> release-trigger
+# If you want to test the deployment, you may optionally add a suffix to the docker image tag
+echo "tagSuffix=" >> release-trigger
 
-if [[ ${2} == "false" ]]; then
-  echo "version=${1}" >> release-trigger-latest
-fi
-
-#git add .
-#git commit -m"trigger GitHub action docker-publish for ${1}"
-#git push
+git add .
+git commit -m"trigger GitHub action docker-publish for ${1}"
+git push
